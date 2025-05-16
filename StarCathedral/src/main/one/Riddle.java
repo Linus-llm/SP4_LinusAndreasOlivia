90621ed783b9e77ea0ac9bde510b7d36eb6f07cb
Tesselation93 <tessvanslyckspam@gmail.com>
2025-05-16 11:04:27 +0200

    Updated riddle class

--- StarCathedral/src/main/one/Riddle.java
+++ StarCathedral/src/main/one/Riddle.java
@@ -2,8 +2,13 @@ package one;
 import java.util.ArrayList;
 
 public class Riddle {
-    String description;
-    ArrayList<String> answerOptions;
+    // Jeg har tilføjet en ekstra hint istedet for de 3 bogstave hints, kan ændres hvis det er
+    private ArrayList<String> extraHintList;
+    private ArrayList<String> riddleList;
+    private ArrayList<String> answerList;
+    private ArrayList<String> hintList;
+    loadRiddles();
+
 
     public Riddle(String description, ArrayList<String> answerOptions){
     this.description = description;
