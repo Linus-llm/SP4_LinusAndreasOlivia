711af4c79b46052b4fabf45eff69194a364d7e74
AndreasRWilliams <iicej0ker97@gmail.com>
2025-05-16 10:31:33 +0200

    Item klassen

--- StarCathedral/src/main/java/org/oliviasFX/Item.java
+++ StarCathedral/src/main/java/org/oliviasFX/Item.java
@@ -4,6 +4,15 @@ public abstract class Item {
     protected String itemName;
     protected String itemDescription;
 
+    public Item(String itemDescription) {
+        this(itemDescription, extractNameFromDescription(itemDescription));
+    }
+
+    public Item(String itemDescription, String itemName) {
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+    }
+
     protected String getItemName() {
         return itemName;
     }
@@ -14,5 +23,18 @@ public abstract class Item {
 
     protected void monsterReward() {
 
+    }
+
+    @Override
+    public String toString() {
+        return itemDescription;
+    }
+
+    private static String extractNameFromDescription(String description) {
+        int lastSpace = description.lastIndexOf(' ');
+        if (lastSpace == -1) {
+            return description;
+        }
+        return description.substring(lastSpace + 1);
     }
 }
