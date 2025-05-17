98ca2849b951f703c8006e9918f47801d6623bdc
AndreasRWilliams <iicej0ker97@gmail.com>
2025-05-17 14:44:51 +0200

    Sikret forbindelse mellem Player, room, Consumables og gear klassen

--- StarCathedral/src/main/java/org/oliviasFX/Consumables.java
+++ StarCathedral/src/main/java/org/oliviasFX/Consumables.java
@@ -1,10 +1,31 @@
 package org.oliviasFX;
 
+
 public class Consumables extends Item {
     private int healingStrength;
 
-    protected int healingCalc() {
 
+    protected int healingCalc() {
         return healingStrength;
     }
+
+
+    public Consumables(String itemDescription, int healingStrength) {
+        super(itemDescription);
+        this.healingStrength = healingStrength;
+    }
+
+
+    public Consumables(String itemDescription, String itemName, int healingStrength) {
+        super(itemDescription, itemName);
+        this.healingStrength = healingStrength;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() + " (Heals: " + healingStrength + ")";
+    }
 }
+
+
