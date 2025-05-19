8491a13e3b0da5accff836da62c1e4606703e6fc
Tesselation93 <tessvanslyckspam@gmail.com>
2025-05-19 11:15:48 +0200

    Add Riddle class

--- StarCathedral/src/main/one/Riddle.java
+++ StarCathedral/src/main/one/Riddle.java
@@ -2,11 +2,10 @@ package main.one;
 import java.util.ArrayList;
 
 public class Riddle {
-    // Jeg har tilføjet en ekstra hint istedet for de 3 bogstave hints, kan ændres hvis det er
-    private ArrayList<String> extraHintList;
-    private ArrayList<String> riddleList;
-    private ArrayList<String> answerList;
-    private ArrayList<String> hintList;
+    private final ArrayList<String> extraHintList;
+    private final ArrayList<String> riddleList;
+    private final ArrayList<String> answerList;
+    private final ArrayList<String> hintList;
 
     // Index of the current riddle
     private int currentIndex = 0;
